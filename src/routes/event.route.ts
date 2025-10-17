b033e3c12f1e02bc2dcf92eaf8faee0d1f4dda8e
Antonio Navarro <dojeDulDev@Proton.me>
2025-10-17 12:53:23 +0200

    feat: get all events

--- src/routes/event.route.ts
+++ src/routes/event.route.ts
@@ -1,10 +1,11 @@
 import  express  from 'express'
-import  {newEvent, loadEvents} from '../controllers/event.controller'
+import  {newEvent, loadEvents, getAllEvents} from '../controllers/event.controller'
 
 const eventRouter = express.Router()
 
 eventRouter.post('/', newEvent)
 eventRouter.post('/loadEvents', loadEvents)
+eventRouter.get('/', getAllEvents)
 
 export default eventRouter
 
